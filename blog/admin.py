aabec6007141c46589fc4773c6bf8cb1b2c8a6c5
Siddharth <siddharthhacker@gmail.com>
2013-01-13 11:23:40 +0530

    Admin Panel is complete! Added search using default django search and improvised looks

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from blog.models import Blog
+from blog.models import Blog,User,Comment
 
-admin.site.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+	list_display = ('title','author_id','pub_date')
+	search_fields=['title']
+	date_hierarchy = 'pub_date'
+	fieldsets = [
+		(None,{'fields':['title','content']}),
+		('Blog Info',{'fields':['author_id','pub_date'],'classes':['collapse']}),
+		]
+admin.site.register(Blog,BlogAdmin)
+admin.site.register(User)
+admin.site.register(Comment)
