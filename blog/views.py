66cb625ae4bafd438f3f227285f0d626a0a5020c
Siddharth <siddharthhacker@gmail.com>
2013-01-24 23:42:50 +0530

    Added basic comments displaying for single entry page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.http import HttpResponse,Http404
-from blog.models import Blog
+from blog.models import Blog,Comment
 
 def index(request):
 	latest_blog_list=Blog.objects.all().order_by('-pub_date')[:5]
@@ -12,7 +12,8 @@ def index(request):
 def entry(request,blog_id):
 	try:
 		blog=Blog.objects.get(id=blog_id)
+		comments=Comment.objects.filter(parent=blog)
 	except Blog.DoesNotExist:
 		raise Http404
-	return render_to_response('blog/entry.html',{'blog':blog,})
+	return render_to_response('blog/entry.html',{'blog':blog,'comments':comments,})
 
